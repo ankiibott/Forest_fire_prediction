fd8789109f45e3bd00dcbec4de01d37a885da6ef
ankiibott <ankii93599@gmail.com>
2025-10-18 11:06:08 +0530

    broken UI issue

--- UI_page/main.jsx
+++ UI_page/main.jsx
@@ -1,8 +1,8 @@
-// main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // Note the change from Frontend.jsx to App.jsx
+import App from './App.jsx'; // Imports the main component
 
+// Mount the React App component into the HTML element with id="root"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
